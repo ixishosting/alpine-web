c6991f051275b8b2f428a3b321eb4bf868d2476a
michael austin <ma@ixis.co.uk>
2016-04-08 15:36:18 +0100

    remove sensistive files from repo pulled in

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ then
     ln -s /assets /app/sites/default/files
 fi
 
+rm /app/docker-compose.yml rancher-compose.yml .drone.yml
+
 chown -Rf apache:apache /assets
 
 # start apache server in foreground mode
